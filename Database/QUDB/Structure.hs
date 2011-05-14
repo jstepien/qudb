30681b5208b563a5f7a7c696f58a3e8e69d706c9
Monar <piotr@monarski.pl>
2011-05-14 23:11:08 +0200

    Move data definitions from Query to Structure

--- Database/QUDB/Structure.hs
+++ Database/QUDB/Structure.hs
@@ -18,6 +18,43 @@ data Column = Column String Type
 -- |Row consists of a list of values.
 data Row = Row [Value]
 
+-- |Defines order.
+data Order = Descending | Ascending
+
+-- |This data type is used in Where QOperation to allow complex logic expressions.
+data WhereConditions =
+          Condition Integer (Value->Bool)   -- Define column id,
+                                            -- value comparer, taking value
+                                            -- from each row of given column id
+                                            -- and returning True or False.
+                    
+        | OrConditions [WhereConditions]    -- Or logic sum
+        | AndConditions [WhereConditions]   -- And logic sum
+
+-- |Available query operations as a part of query send to data base.
+data QOperation = Select | Update | Insert | Delete | From | Where | OrderBy | Top
+
+-- |Arguments of QOperation, each of them is related only to one operation.
+data QArg = SelectArg [String]            -- column names to return.
+          | UpdateArg [(String, Value)]   -- column names and a value assigned to them.
+          | InsertArg [Row]               -- rows to insert.
+          | DeleteArg    
+          | FromArg Table               
+          | WhereArg WhereConditions      -- read above.
+          | OrderByArg [(Integer, Order)] -- column id and applied order.
+          | TopArg Integer                -- number of top rows to return.
+
+-- |Query part defined by QOpetation and QArg.
+data Query = Query QOperation QArg
+
+-- |QTable is representation of db Table with extra data. Is used when processing query.
+-- |qTable is original table,
+-- |qRows are selected rows and notQRows is list of other Table rows.
+data QTable = QTable {qTable::Table, qRows, notQRows::[Row]} | EmptyQTable
+
+class ExecutableQuery eq where
+    exeq :: DB -> eq -> QTable -> QTable
+
 -- |Creates a new DB instance with a given filename.
 initDB :: String -> IO DB
 initDB filename = do
