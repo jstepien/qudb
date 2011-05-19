e1a44849a353676bf44b26ae77c45b1cc87e9000
Jan Stępień <jstepien@users.sourceforge.net>
2011-05-19 23:19:18 +0200

    QUDB exports Value data constructors
    Without IntValue and StringValue extracting values stored in the database
    doesn't seem to be easy.

--- Database/QUDB.hs
+++ Database/QUDB.hs
@@ -1,7 +1,8 @@
-module Database.QUDB (S.initDB, S.loadDB, S.dumpDB, query, S.DB) where
+module Database.QUDB (S.initDB, S.loadDB, S.dumpDB, query, S.DB,
+    Value(IntValue, StringValue)) where
 
 import qualified Database.QUDB.Structure as S
-import Database.QUDB.EntityTypes (Value)
+import Database.QUDB.EntityTypes (Value(IntValue, StringValue))
 import Database.QUDB.Parser (parse)
 
 query :: S.DB -> String -> IO [[Value]]
